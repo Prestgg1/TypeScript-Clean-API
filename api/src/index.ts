cac0567a0b14d0ceb1a9eb4dac374c14f0e50f98
Prestgg1 <prestgg12@gmail.com>
2026-03-12 10:16:40 +0400

    Added Cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,23 +1,29 @@
 import "reflect-metadata";
 import { RegisterRoutes } from "./routes";
 import swaggerUi from "swagger-ui-express";
-import type { Response as ExResponse, Request as ExRequest, Response } from "express";
+import type { Response, Request } from "express";
 import express, { json, urlencoded } from "express";
 import swaggerDocument from "../build/swagger.json";
 import { apiReference } from "@scalar/express-api-reference";
-
+import cors from "cors";
 export const app = express();
 
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
-app.get("/swagger.json", (_req: ExRequest, res: ExResponse) => {
+app.use(cors())
+
+app.get("/swagger", (_req: Request, res: Response) => {
+  return res.json({ "Hello": "true" });
+});
+
+app.get("/swagger.json", (_req: Request, res: Response) => {
   return res.json(swaggerDocument);
 });
 
 RegisterRoutes(app);
 
-app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+app.use("/docs", swaggerUi.serve, async (_req: Request, res: Response) => {
   return res.send(
     swaggerUi.generateHTML(await import("../build/swagger.json"), {
       customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css",
@@ -28,13 +34,12 @@ app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
     })
   );
 });
+/*
 app.use("/scalar", apiReference({
-  spec: {
-    content: swaggerDocument,
-  },
+  
 }));
 
-
+*/
 app.get("/", (_, res: Response) => {
 
   return res.send(`<h1>Salamlar, ASKORG saytı hazırlanır.</h1>`);
